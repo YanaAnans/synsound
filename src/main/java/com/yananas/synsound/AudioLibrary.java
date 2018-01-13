2fbcc317100dfe7afce9bbc9d72670568956a8cf
Andrey Beskrovnykh <andrey.beskrovnykh@yandex.ru>
2018-01-13 22:40:27 +0700

    Fix style, notes, etc.

--- src/main/java/com/yananas/synsound/AudioLibrary.java
+++ src/main/java/com/yananas/synsound/AudioLibrary.java
@@ -1,28 +1,19 @@
 package com.yananas.synsound;
 
-import java.io.IOException;
-
 public class AudioLibrary {
 
-	private static double[] createNote(double duration, double frequency) {
-		double amplitude = 0.8;
-		double[] frequencies = new double[(int) (duration * AudioSettings.SAMPLE_RATE)];
-		for (int sampleId = 0; sampleId < frequencies.length; sampleId++) {
-			duration = sampleId / AudioSettings.SAMPLE_RATE;
-			frequencies[sampleId] = amplitude * Math.sin(2 * Math.PI * frequency * duration);
-		}
-		return frequencies;
-	}
+    private static double[] createNote(double duration, double frequency) {
+        double amplitude = 0.8;
+        double[] frequencies = new double[(int) (duration * AudioSettings.SAMPLE_RATE)];
+        for (int sampleId = 0; sampleId < frequencies.length; sampleId++) {
+            double time = sampleId / AudioSettings.SAMPLE_RATE;
+            frequencies[sampleId] = amplitude * Math.sin(2 * Math.PI * frequency * time);
+        }
+        return frequencies;
+    }
 
-	public static void main(String[] args) {
-		try {
-			String filename = "simple_wave.wav";
-			AudioUtils.save(filename, createNote(2.0, 1245));
-			AudioPlayer player = new AudioPlayer();
-			player.playSound(filename);
-		} catch (IllegalArgumentException | IOException e) {
-			e.printStackTrace();
-		}
-	}
-
+    public static void main(String[] args) {
+        AudioPlayer player = new AudioPlayer();
+        player.play(createNote(2.0, 440.0));
+    }
 }
--- src/main/java/com/yananas/synsound/AudioPlayer.java
+++ src/main/java/com/yananas/synsound/AudioPlayer.java
@@ -7,70 +7,77 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
 public class AudioPlayer {
 
-	private final int BUFFER_SIZE = 128000;
-	private File soundFile;
-	private AudioInputStream audioStream;
-	private AudioFormat audioFormat;
-	private SourceDataLine sourceLine;
+    private final int BUFFER_SIZE = 1280000;
 
-	/**
-	 * @param filename
-	 *            the name of the file that is going to be played
-	 */
-	public void playSound(String filename) {
+    private File soundFile;
+    private AudioInputStream audioStream;
+    private AudioFormat audioFormat;
+    private SourceDataLine sourceLine;
 
-		String strFilename = filename;
+    /**
+     * Play sound represented by an array of samples
+     * @param samples
+     *          an array of samples that is going to be played
+     * */
+    public void play(double[] samples) {
+        try {
+            new File("tmp").mkdir();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        try {
+            String filename = "tmp/sound.wav";
+            AudioUtils.save(filename, samples);
+            play(filename);
+        } catch (IllegalArgumentException|IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		try {
-			soundFile = new File(strFilename);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+    /**
+     * Play sound file given by its filename
+     * @param filename
+     *            the name of the file that is going to be played
+     */
+    public void play(String filename) {
+        try {
+            soundFile = new File(filename);
+            audioStream = AudioSystem.getAudioInputStream(soundFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-		try {
-			audioStream = AudioSystem.getAudioInputStream(soundFile);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
+        audioFormat = audioStream.getFormat();
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        try {
+            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+            sourceLine.open(audioFormat);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
-		audioFormat = audioStream.getFormat();
-
-		DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
-		try {
-			sourceLine = (SourceDataLine) AudioSystem.getLine(info);
-			sourceLine.open(audioFormat);
-		} catch (LineUnavailableException e) {
-			e.printStackTrace();
-			System.exit(1);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-
-		sourceLine.start();
-
-		int nBytesRead = 0;
-		byte[] abData = new byte[BUFFER_SIZE];
-		while (nBytesRead != -1) {
-			try {
-				nBytesRead = audioStream.read(abData, 0, abData.length);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			if (nBytesRead >= 0) {
-				@SuppressWarnings("unused")
-				int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
-			}
-		}
-
-		sourceLine.drain();
-		sourceLine.close();
-	}
+        sourceLine.start();
+        int nBytesRead = 0;
+        byte[] abData = new byte[BUFFER_SIZE];
+        while (nBytesRead != -1) {
+            try {
+                nBytesRead = audioStream.read(abData, 0, abData.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (nBytesRead >= 0) {
+                @SuppressWarnings("unused")
+                int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+            }
+        }
+        sourceLine.drain();
+        sourceLine.close();
+    }
 }
