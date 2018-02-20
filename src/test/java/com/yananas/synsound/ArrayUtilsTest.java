3b68f4bfe755b990f468b39208c23c63feb9a925
Andrey Beskrovnykh <andrey.beskrovnykh@yandex.ru>
2018-02-20 19:06:03 +0700

    Fix formatting

--- src/test/java/com/yananas/synsound/ArrayUtilsTest.java
+++ src/test/java/com/yananas/synsound/ArrayUtilsTest.java
@@ -6,31 +6,31 @@ import static com.yananas.synsound.ArrayUtils.clip;
 import static org.junit.Assert.assertArrayEquals;
 
 public class ArrayUtilsTest {
-	@Test
-	public void clipLast2Test() {
-		double[] in = new double[] {1, 2, 3, 4};
-		double[] expected = new double[] {3, 4};
-		assertArrayEquals(expected, clip(in, 2, in.length), Constants.EPS);
-	}
+    @Test
+    public void clipLast2Test() {
+        double[] in = new double[] { 1, 2, 3, 4 };
+        double[] expected = new double[] { 3, 4 };
+        assertArrayEquals(expected, clip(in, 2, in.length), Constants.EPS);
+    }
 
-	@Test
-	public void clipLast3Test() {
-		double[] in = new double[] {1, 2, 3, 4, 5};
-		double[] expected = new double[] {3, 4, 5};
-		assertArrayEquals(expected, clip(in, 2, in.length), Constants.EPS);
-	}
+    @Test
+    public void clipLast3Test() {
+        double[] in = new double[] { 1, 2, 3, 4, 5 };
+        double[] expected = new double[] { 3, 4, 5 };
+        assertArrayEquals(expected, clip(in, 2, in.length), Constants.EPS);
+    }
 
-	@Test
-	public void clipFirst2Test() {
-		double[] in = new double[] {1, 2, 3};
-		double[] expected = new double[] {1, 2};
-		assertArrayEquals(expected, clip(in, 0, 1), Constants.EPS);
-	}
+    @Test
+    public void clipFirst2Test() {
+        double[] in = new double[] { 1, 2, 3 };
+        double[] expected = new double[] { 1, 2 };
+        assertArrayEquals(expected, clip(in, 0, 1), Constants.EPS);
+    }
 
-	@Test
-	public void clipFirst3Test() {
-		double[] in = new double[] {1, 2, 3, 4};
-		double[] expected = new double[] {1, 2, 3};
-		assertArrayEquals(expected, clip(in, 0, 2), Constants.EPS);
-	}
+    @Test
+    public void clipFirst3Test() {
+        double[] in = new double[] { 1, 2, 3, 4 };
+        double[] expected = new double[] { 1, 2, 3 };
+        assertArrayEquals(expected, clip(in, 0, 2), Constants.EPS);
+    }
 }
