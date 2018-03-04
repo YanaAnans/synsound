63fa25ad4f48c787eb7b37c1440e759f6b2d947c
YanaAnans <32519578+YanaAnans@users.noreply.github.com>
2018-03-04 21:09:38 +0700

    Remove deprecated methods

--- src/main/java/com/yananas/synsound/ArrayUtils.java
+++ src/main/java/com/yananas/synsound/ArrayUtils.java
@@ -2,56 +2,48 @@ package com.yananas.synsound;
 
 public class ArrayUtils {
 
-    public static double[] clip(double[] array, int start, int end) {
-        int n = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (i > end) {
-                n = n + 1;
-            }
-        }
-        double[] array2 = new double[array.length - start - n];
-        System.arraycopy(array, start, array2, 0, array.length - start - n);
-        return array2;
-    }
+	public static double[] clip(double[] array, int start, int end) {
+		int n = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (i > end) {
+				n = n + 1;
+			}
+		}
+		double[] array2 = new double[array.length - start - n];
+		System.arraycopy(array, start, array2, 0, array.length - start - n);
+		return array2;
+	}
 
-    public static double[] repeat(double[] array, int times) {
-        double[] array2 = new double[array.length * times];
-        for (int i = 0; i < times; i++) {
-            System.arraycopy(array, 0, array2, array.length * i, array.length);
-        }
-        return array2;
-    }
+	public static double[] repeat(double[] array, int times) {
+		double[] array2 = new double[array.length * times];
+		for (int i = 0; i < times; i++) {
+			System.arraycopy(array, 0, array2, array.length * i, array.length);
+		}
+		return array2;
+	}
 
-    @Deprecated
-    public static double[] union(double[] array1, double[] array2) {
-        double[] result = new double[array1.length + array2.length];
-        System.arraycopy(array1, 0, result, 0, array1.length);
-        System.arraycopy(array2, 0, result, array1.length, array2.length);
-        return result;
-    }
+	public static double max(double[] array) {
+		double max = 0;
+		for (double anArray : array) {
+			max = (max > anArray) ? max : anArray;
+		}
+		return max;
+	}
 
-    public static double max(double[] array) {
-        double max = 0;
-        for (double anArray : array) {
-            max = (max > anArray) ? max : anArray;
-        }
-        return max;
-    }
+	public static double min(double[] array) {
+		double min = Double.MAX_VALUE;
+		for (double anArray : array) {
+			min = (min < anArray) ? min : anArray;
+		}
+		return min;
+	}
 
-    public static double min(double[] array) {
-        double min = Double.MAX_VALUE;
-        for (double anArray : array) {
-            min = (min < anArray) ? min : anArray;
-        }
-        return min;
-    }
-
-    public static double[] range(double min, double max, int size) {
-        double segment = (max - min) / (size - 1);
-        double[] range = new double[size];
-        for (int i = 0; i < size; i++) {
-            range[i] = min + segment * i;
-        }
-        return range;
-    }
+	public static double[] range(double min, double max, int size) {
+		double segment = (max - min) / (size - 1);
+		double[] range = new double[size];
+		for (int i = 0; i < size; i++) {
+			range[i] = min + segment * i;
+		}
+		return range;
+	}
 }
