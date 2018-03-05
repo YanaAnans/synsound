02e69b0417c3bfa68ad6b4032688418280c25139
YanaAnans <32519578+YanaAnans@users.noreply.github.com>
2018-03-05 21:43:33 +0700

    Words

--- src/main/java/com/yananas/synsound/experiments/Words.java
+++ src/main/java/com/yananas/synsound/experiments/Words.java
@@ -7,21 +7,25 @@ import com.yananas.synsound.AudioLibrary;
 import com.yananas.synsound.AudioPlayer;
 import com.yananas.synsound.model.WavData;
 
-public class Word {
+public class Words {
 
     public static void main(String[] args) {
         AudioEditor audioEditor = new AudioEditor();
         WavData wavData1 = AudioLibrary.voice("papap.wav");
-        wavData1 = AudioEditor.clip(wavData1, 354, 2020);
+        wavData1 = AudioEditor.clip(wavData1, 350, 2052);
         WavData wavData2 = AudioLibrary.voice("r_ir_ir_.wav");
-        wavData2 = AudioEditor.clip(wavData2, 1615, 750);
+        wavData2 = AudioEditor.clip(wavData2, 1610, 1310);
         WavData wavData3 = AudioLibrary.voice("v_iv_iv_.wav");
-        wavData3 = AudioEditor.clip(wavData3, 1237, 1480);
+        wavData3 = AudioEditor.clip(wavData3, 1331, 1500);
         WavData wavData4 = AudioLibrary.voice("v_ev_ev_.wav");
-        wavData4 = AudioEditor.clip(wavData4, 1225, 750);
+        wavData4 = AudioEditor.clip(wavData4, 1261, 1100);
         WavData wavData5 = AudioLibrary.voice("tetet.wav");
-        wavData5 = AudioEditor.clip(wavData5, 2260, 50);
-        WavData joinedData = audioEditor.union(Arrays.asList(wavData1, wavData2, wavData3, wavData4, wavData5));
+        wavData5 = AudioEditor.clip(wavData5, 2300, 350);
+        WavData wavData6 = AudioLibrary.voice("m_im_im_.wav");
+        wavData6 = AudioEditor.clip(wavData6, 460, 1950);
+        WavData wavData7 = AudioLibrary.voice("ryr_ir.wav");
+        wavData7 = AudioEditor.clip(wavData7, 2490, 80);
+        WavData joinedData = audioEditor.union(Arrays.asList(wavData1, wavData2, wavData3, wavData4, wavData5, wavData6, wavData7));
         AudioPlayer player = new AudioPlayer();
         player.play(joinedData);
 
