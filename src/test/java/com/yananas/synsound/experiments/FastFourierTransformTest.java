75e4211b6a6dd6f29867744e1408fde3e4455d1c
Andrey Beskrovnykh <andrey.beskrovnykh@yandex.ru>
2018-03-11 22:26:26 +0700

    Cleanup FastFourierTransformTest.java and setup it for the task

--- src/test/java/com/yananas/synsound/experiments/FastFourierTransformTest.java
+++ src/test/java/com/yananas/synsound/experiments/FastFourierTransformTest.java
@@ -1,15 +1,6 @@
 package com.yananas.synsound.experiments;
 
-import org.apache.commons.math3.complex.Complex;
-import org.apache.commons.math3.transform.DftNormalization;
-import org.apache.commons.math3.transform.FastFourierTransformer;
-import org.apache.commons.math3.transform.TransformType;
 import org.junit.Test;
-
-import com.yananas.synsound.ArrayUtils;
-import com.yananas.synsound.math.MathPlot;
-import com.yananas.synsound.math.PiecewiseFunction;
-import com.yananas.synsound.math.Waveform;
 
 public class FastFourierTransformTest {
     /*
@@ -38,32 +29,13 @@ public class FastFourierTransformTest {
         // TODO: 1) prepare test data, 2) call FFT method and 3) assert results
     }
 
-    public static void main(String[] args) {
-        Waveform wf = Waveform.builder()
-                .amplitude(5.0).frequency(1.0)
-                .build();
-
-        PiecewiseFunction pf = PiecewiseFunction.builder()
-                .left(0.0).right(1.0).numberOfPieces(255).fn(wf.fn())
-                .build();
-
-        MathPlot.plot("waveform", "time", "amplitude", pf.getXValues(), pf.getYValues());
-
-        FastFourierTransformer transformer = new FastFourierTransformer(DftNormalization.STANDARD);
-
-        Complex[] result = transformer.transform(pf.getYValues(), TransformType.FORWARD);
-
-        double[] absValues = ArrayUtils.fromComplexAbs(result);
-        MathPlot.plot("Absolute values", absValues);
-
-        double[] imaginaryParts = ArrayUtils.fromComplexImaginaryParts(result);
-        MathPlot.plot("Imaginary parts", imaginaryParts);
-
-        double[] realParts = ArrayUtils.fromComplexRealParts(result);
-        MathPlot.plot("Real parts", realParts);
-
-        double[] angles = ArrayUtils.fromComplexArgs(result);
-        MathPlot.plot("Angles", angles);
+    /*
+     * The input is a sequence of real numbers of length 8.
+     * The first element is 1, the rest is 0.
+     * The output should be a sequence of 1-s of the same length
+     * */
+    @Test
+    public void testConstSequence() {
+        // TODO: 1) prepare test data, 2) call FFT method and 3) assert results
     }
-
 }
