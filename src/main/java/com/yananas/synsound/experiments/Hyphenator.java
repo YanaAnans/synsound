7ca39507b682c10ac760af2860a74be8a95cf74d
YanaAnans <32519578+YanaAnans@users.noreply.github.com>
2018-05-08 23:28:13 +0700

    Правки

--- src/main/java/com/yananas/synsound/experiments/Hyphenator.java
+++ src/main/java/com/yananas/synsound/experiments/Hyphenator.java
@@ -5,45 +5,15 @@ import java.util.regex.Pattern;
 
 public class Hyphenator {
 
-    private final static String vowels = "АаИиУуЭэОоЫыЯяЮюЕеЁё";
-    private final static String y = "ЯяЮюЕеЁё";
-    private final static String consonants = "БбВвГгДдЖжЗзКкЛлМмНнПпРрСсТтФфХхЦцЧчШшЩщЙй";
-    private final static String x = "ЙйЪъЬь";
+    private final static String vowels = "аиуэоыяюеё";
+    private final static String ioVowels = "яюеё";
+    private final static String consonants = "бвгджзклмнпрстфхцчшщй";
+    private final static String endOfSyl = "йъь";
 
-    public static void firstRegexp(String text) {
-        text = text.replaceAll("([0-9]+)([^0-9]+)", "$1-$2");
-        text = text.replaceAll("([^0-9]+)([0-9]+)", "$1-$2");
-        System.out.println(text);
-    }
+    private static ArrayList<String> patterns = new ArrayList<String>();
 
-    public static void twoVowels(String text) {
-        text = text.replaceAll("([" + vowels + "])([" + vowels + "])", "$1-$2");
-        System.out.println(text);
-    }
-
-    public static void oneVowelTwoCons(String text) {
-        Pattern p = Pattern.compile("([" + consonants + "])([" + vowels + "])([" + consonants + "])");
-        text = text.replaceAll(p.toString(), "$1$2-$2$3");
-        System.out.println(text);
-    }
-
-    public static void twoCons(String text) {
-        Pattern p = Pattern.compile("([" + consonants + "])([" + consonants + "])");
-        text = text.replaceAll(p.toString(), "$1-$2");
-        System.out.println(text);
-    }
-
-    public static void hyphen(String text) {
-        Pattern p1 = Pattern.compile("([" + consonants + "])([" + consonants + "])");
-        text = text.replaceAll(p1.toString(), "$1-$2");
-        Pattern p2 = Pattern.compile("([" + consonants + "])([" + vowels + "])([" + consonants + "])");
-        text = text.replaceAll(p2.toString(), "$1$2-$2$3");
-        System.out.println(text);
-    }
-
-    public static void hyphens(String text) {
-        ArrayList<String> patterns = new ArrayList<String>();
-        patterns.add("([" + x + "])([" + y + "])");
+    public Hyphenator() {
+        patterns.add("([" + endOfSyl + "])([" + ioVowels + "])");
         patterns.add("([" + vowels + "])([" + vowels + "])");
         patterns.add("([" + vowels + consonants + "])([" + consonants + "][" + vowels + "])");
         patterns.add("([" + consonants + "][" + vowels + "])([" + consonants + "][" + vowels + "])");
@@ -51,20 +21,31 @@ public class Hyphenator {
                 "([" + vowels + "][" + consonants + "])([" + consonants + "][" + consonants + "][" + vowels + "])");
         patterns.add("([" + vowels + "][" + consonants + "][" + consonants + "])([" + consonants + "][" + consonants
                 + "][" + vowels + "])");
+    }
 
+    public static String hyphen(String text) {
+        Pattern p1 = Pattern.compile("([" + consonants + "])([" + consonants + "])");
+        text = text.replaceAll(p1.toString(), "$1-$2");
+        Pattern p2 = Pattern.compile("([" + consonants + "])([" + vowels + "])([" + consonants + "])");
+        text = text.replaceAll(p2.toString(), "$1$2-$2$3");
+        return text;
+    }
+
+    public static String hyphens(String text) {
         for (int i = 0; i < patterns.size(); i++) {
             Pattern p = Pattern.compile(patterns.get(i));
+            text = text.toLowerCase();
             text = text.replaceAll(p.toString(), "$1-$2");
         }
-        System.out.println(text);
+        return text;
     }
 
     public static void main(String[] args) {
-        hyphens("параллелепипед");
-        hyphens("йогурт");
-        hyphens("покой");
-        hyphens("строка");
-        hyphens("подъезд");
-        hyphens("вьюн");
+        System.out.println(hyphens("параллелепипед"));
+        System.out.println(hyphens("подъезд"));
+        System.out.println(hyphens("СНЕГ"));
+        System.out.println(hyphens("овраг"));
+        System.out.println(hyphens("ВьЮн"));
+        System.out.println(hyphens("строка"));
     }
 }
