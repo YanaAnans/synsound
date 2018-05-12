b248ec40a339afa38fda69f321ec7657bd60ae66
Andrey Beskrovnykh <andrey.beskrovnykh@yandex.ru>
2018-05-12 12:40:30 +0700

    Help yananas to fix review issues

--- src/main/java/com/yananas/synsound/experiments/Hyphenator.java
+++ src/main/java/com/yananas/synsound/experiments/Hyphenator.java
@@ -1,6 +1,7 @@
 package com.yananas.synsound.experiments;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 public class Hyphenator {
@@ -10,20 +11,19 @@ public class Hyphenator {
     private final static String consonants = "бвгджзклмнпрстфхцчшщй";
     private final static String endOfSyl = "йъь";
 
-    private static ArrayList<String> patterns = new ArrayList<String>();
+    private HashMap<String, String> rules = new HashMap<String, String>();
 
     public Hyphenator() {
-        patterns.add("([" + endOfSyl + "])([" + ioVowels + "])");
-        patterns.add("([" + vowels + "])([" + vowels + "])");
-        patterns.add("([" + vowels + consonants + "])([" + consonants + "][" + vowels + "])");
-        patterns.add("([" + consonants + "][" + vowels + "])([" + consonants + "][" + vowels + "])");
-        patterns.add(
-                "([" + vowels + "][" + consonants + "])([" + consonants + "][" + consonants + "][" + vowels + "])");
-        patterns.add("([" + vowels + "][" + consonants + "][" + consonants + "])([" + consonants + "][" + consonants
-                + "][" + vowels + "])");
+        rules.put("([" + endOfSyl + "])([" + ioVowels + "])", "$1-$2");
+        rules.put("([" + vowels + "])([" + vowels + "])", "$1-$2");
+        rules.put("([" + vowels + consonants + "])([" + consonants + "][" + vowels + "])", "$1-$2");
+        rules.put("([" + consonants + "][" + vowels + "])([" + consonants + "][" + vowels + "])", "$1-$2");
+        rules.put("([" + vowels + "][" + consonants + "])([" + consonants + "][" + consonants + "][" + vowels + "])", "$1-$2");
+        rules.put("([" + vowels + "][" + consonants + "][" + consonants + "])([" + consonants + "][" + consonants
+                + "][" + vowels + "])", "$1-$2");
     }
 
-    public static String hyphen(String text) {
+    public String hyphen(String text) {
         Pattern p1 = Pattern.compile("([" + consonants + "])([" + consonants + "])");
         text = text.replaceAll(p1.toString(), "$1-$2");
         Pattern p2 = Pattern.compile("([" + consonants + "])([" + vowels + "])([" + consonants + "])");
@@ -31,21 +31,23 @@ public class Hyphenator {
         return text;
     }
 
-    public static String hyphens(String text) {
-        for (int i = 0; i < patterns.size(); i++) {
-            Pattern p = Pattern.compile(patterns.get(i));
-            text = text.toLowerCase();
-            text = text.replaceAll(p.toString(), "$1-$2");
+    public String hyphens(String in) {
+        String res = in.toLowerCase();
+        for (Entry<String, String> rule : rules.entrySet()) {
+            res = res.replaceAll(rule.getKey(), rule.getValue());
         }
-        return text;
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(hyphens("параллелепипед"));
-        System.out.println(hyphens("подъезд"));
-        System.out.println(hyphens("СНЕГ"));
-        System.out.println(hyphens("овраг"));
-        System.out.println(hyphens("ВьЮн"));
-        System.out.println(hyphens("строка"));
+        Hyphenator hyphenator = new Hyphenator();
+        System.out.println(hyphenator.hyphen("шторм"));
+        System.out.println(hyphenator.hyphen("принц"));
+        System.out.println(hyphenator.hyphens("параллелепипед"));
+        System.out.println(hyphenator.hyphens("подъезд"));
+        System.out.println(hyphenator.hyphens("СНЕГ"));
+        System.out.println(hyphenator.hyphens("овраг"));
+        System.out.println(hyphenator.hyphens("ВьЮн"));
+        System.out.println(hyphenator.hyphens("строка"));
     }
 }
